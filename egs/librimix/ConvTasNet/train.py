eb4c0cd6a55d54ba803040d169aba830768f37b5
ZhaZhaFon <zifeng_z@foxmail.com>
2021-11-27 14:52:58 +0800

    2021-11-27 / speech / librimix_convtasnet

--- egs/librimix/ConvTasNet/train.py
+++ egs/librimix/ConvTasNet/train.py
@@ -22,9 +22,16 @@ from asteroid.losses import PITLossWrapper, pairwise_neg_sisdr
 # will limit the number of available GPUs for train.py .
 parser = argparse.ArgumentParser()
 parser.add_argument("--exp_dir", default="exp/tmp", help="Full path to save best validation model")
-
+parser.add_argument("-g", "--gpus", default="0", help="GPU available")
+parser.add_argument("--conf", default="local/conf.yml", help="conf.yml")
 
 def main(conf):
+    
+    print('\n### READY TO GO ! ###\n')
+    from IPython import embed
+    embed()
+    
+    print('>> 读取train_set...')
     train_set = LibriMix(
         csv_dir=conf["data"]["train_dir"],
         task=conf["data"]["task"],
@@ -33,6 +40,7 @@ def main(conf):
         segment=conf["data"]["segment"],
     )
 
+    print('>> 读取val_set...')
     val_set = LibriMix(
         csv_dir=conf["data"]["valid_dir"],
         task=conf["data"]["task"],
@@ -41,6 +49,7 @@ def main(conf):
         segment=conf["data"]["segment"],
     )
 
+    print('>> 加载train_loader...')
     train_loader = DataLoader(
         train_set,
         shuffle=True,
@@ -49,6 +58,7 @@ def main(conf):
         drop_last=True,
     )
 
+    print('>> 加载val_loader...')
     val_loader = DataLoader(
         val_set,
         shuffle=False,
@@ -58,6 +68,7 @@ def main(conf):
     )
     conf["masknet"].update({"n_src": conf["data"]["n_src"]})
 
+    print('>> 加载模型...')
     model = ConvTasNet(
         **conf["filterbank"], **conf["masknet"], sample_rate=conf["data"]["sample_rate"]
     )
@@ -67,6 +78,7 @@ def main(conf):
     if conf["training"]["half_lr"]:
         scheduler = ReduceLROnPlateau(optimizer=optimizer, factor=0.5, patience=5)
     # Just after instantiating, save the args. Easy loading in the future.
+    print('>> 建立&保存实验...')
     exp_dir = conf["main_args"]["exp_dir"]
     os.makedirs(exp_dir, exist_ok=True)
     conf_path = os.path.join(exp_dir, "conf.yml")
@@ -74,6 +86,7 @@ def main(conf):
         yaml.safe_dump(conf, outfile)
 
     # Define Loss function.
+    print('>> 建立system...')
     loss_func = PITLossWrapper(pairwise_neg_sisdr, pit_from="pw_mtx")
     system = System(
         model=model,
@@ -86,28 +99,36 @@ def main(conf):
     )
 
     # Define callbacks
+    print('>> 设置callbacks...')
     callbacks = []
     checkpoint_dir = os.path.join(exp_dir, "checkpoints/")
     checkpoint = ModelCheckpoint(
-        checkpoint_dir, monitor="val_loss", mode="min", save_top_k=5, verbose=True
-    )
-    callbacks.append(checkpoint)
+        checkpoint_dir, monitor="val_loss", mode="min", save_top_k=1, verbose=True, save_last=True)
+    #callbacks.append(checkpoint)
     if conf["training"]["early_stop"]:
         callbacks.append(EarlyStopping(monitor="val_loss", mode="min", patience=30, verbose=True))
 
+    print('>> 使用CUDA {}'.format(conf['main_args']['gpus']))
+    os.environ['CUDA_VISIBLE_DEVICES'] = conf['main_args']['gpus']
+    
     # Don't ask GPU if they are not available.
     gpus = -1 if torch.cuda.is_available() else None
     distributed_backend = "ddp" if torch.cuda.is_available() else None
 
+    print('>> 加载Trainer...')
     trainer = pl.Trainer(
         max_epochs=conf["training"]["epochs"],
         callbacks=callbacks,
+        checkpoint_callback=checkpoint,
         default_root_dir=exp_dir,
         gpus=gpus,
         distributed_backend=distributed_backend,
         limit_train_batches=1.0,  # Useful for fast experiment
         gradient_clip_val=5.0,
     )
+    
+    print('')
+    print('>> 启动trianer.fit()...')
     trainer.fit(system)
 
     best_k = {k: v.item() for k, v in checkpoint.best_k_models.items()}
@@ -122,6 +143,7 @@ def main(conf):
     to_save.update(train_set.get_infos())
     torch.save(to_save, os.path.join(exp_dir, "best_model.pth"))
 
+    torch.cuda.empty_cache()
 
 if __name__ == "__main__":
     import yaml
@@ -131,7 +153,13 @@ if __name__ == "__main__":
     # We start with opening the config file conf.yml as a dictionary from
     # which we can create parsers. Each top level key in the dictionary defined
     # by the YAML file creates a group in the parser.
-    with open("local/conf.yml") as f:
+    
+    args = parser.parse_args()
+    conf_path = args.conf
+    
+    print('')
+    print(f'>> 解析超参数 {conf_path}...')
+    with open(conf_path) as f:
         def_conf = yaml.safe_load(f)
     parser = prepare_parser_from_dict(def_conf, parser=parser)
     # Arguments are then parsed into a hierarchical dictionary (instead of
